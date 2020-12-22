9b2c7bc4c6b4a809ac387bec79d0fff1d1c995d1
disanda <skyanda1@qq.com>
2020-12-22 17:05:18 +0800

    x

--- train_be.py
+++ train_be.py
@@ -8,10 +8,9 @@ import lpips
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
 
-
 def set_seed(seed): #随机数设置
 	np.random.seed(seed)
-	random.seed(seed)
+	#random.seed(seed)
 	torch.manual_seed(seed) # cpu
 	torch.cuda.manual_seed_all(seed)  # gpu
 	torch.backends.cudnn.deterministic = True
