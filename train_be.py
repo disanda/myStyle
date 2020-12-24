a6fcb2c24edc50b9102d75d983061d6b31497759
disanda <skyanda1@qq.com>
2020-12-24 16:06:11 +0800

    x

--- train_be.py
+++ train_be.py
@@ -23,7 +23,7 @@ def train():
 	#Gm.requires_grad_(False)
 	#Gs.requires_grad_(False)
 	E = BE.BE()
-	E.load_state_dict(torch.load('_yucheng/myStyle/myStyle-v1/result/EB_V3_finetuneLoss/models/E_model_ep40000.pth'))
+	E.load_state_dict(torch.load('../E_model_ep40000_bugSy2.pth'))
 	Gs.cuda()
 	Gm.cuda()
 	E.cuda()
