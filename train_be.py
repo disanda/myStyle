1c3f5d84b189e2da0351df3769f2425e4fe008f6
disanda <skyanda1@qq.com>
2020-12-27 15:57:30 +0800

    x

--- train_be.py
+++ train_be.py
@@ -51,7 +51,7 @@ def train(avg_tensor = None, coefs=0):
 	loss_lpips = lpips.LPIPS(net='vgg').to('cuda')
 	loss_kl = torch.nn.KLDivLoss()
 
-	batch_size = 3
+	batch_size = 1
 	const1 = const_.repeat(batch_size,1,1,1)
 	for epoch in range(120000):
 		set_seed(epoch%20000)
@@ -144,8 +144,8 @@ def train(avg_tensor = None, coefs=0):
 		print('-')
 
 		if epoch % 100 == 0:
-			test_img = torch.cat((imgs1[:3],imgs2[:3]))*0.5+0.5
-			torchvision.utils.save_image(test_img, resultPath1_1+'/ep%d.jpg'%(epoch), nrow=3)
+			test_img = torch.cat((imgs1[:1],imgs2[:1]))*0.5+0.5
+			torchvision.utils.save_image(test_img, resultPath1_1+'/ep%d.jpg'%(epoch)) # nrow=3
 			with open(resultPath+'/Loss.txt', 'a+') as f:
 				print('i_'+str(epoch)+'--loss_all__:'+str(loss_all.item())+'--loss_mse:'+str(loss_img_mse.item())+'--loss_lpips:'+str(loss_img_lpips.item())+'--loss_kl_img:'+str(loss_kl_img.item()),file=f)
 				print('loss_img_mse_column:'+str(loss_img_mse_column.item())+'loss_img_lpips_column:'+str(loss_img_lpips_column.item())\
